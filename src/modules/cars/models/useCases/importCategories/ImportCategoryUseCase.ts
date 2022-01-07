5eaf4b7e947d41e7d758f9fac95fe2f8cab1752a
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-01-07 10:22:44 -0300

    update UseCases

---
+++ src/modules/cars/models/useCases/importCategories/ImportCategoryUseCase.ts
@@ -0,0 +1,36 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+import { ICategoriesRepository } from "../../../repositories/ICategoriesRepository";
+
+interface IImportCategory{
+    name: string;
+    description: string;
+}
+
+class ImportCategoryUseCase{
+    constructor(private categoriesRepository: ICategoriesRepository){}
+
+    loadCategories(file: Express.Multer.File){
+        const stream = fs.createReadStream(file.path);
+        const categories: IImportCategory[] = [];
+        
+        const parseFile = csvParse();
+        
+        stream.pipe(parseFile)
+
+        parseFile.on("data", async (line) => {
+            const [name, description] = line;
+
+            categories.push({
+                name,
+                description,
+            });
+        });
+    }
+
+    execute(file: Express.Multer.File): void {
+        const categories = this.loadCategories(file);
+    }
+}
+
+export {ImportCategoryUseCase}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { createCategoryController } from '../modules/cars/models/useCases/CreateCategory';
+import { importCategoryController } from '../modules/cars/models/useCases/importCategories';
 import { listCategoriesController } from '../modules/cars/models/useCases/ListCategories';
 
 const categoriesRoutes = Router();
+const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res);
@@ -10,6 +14,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (req, res) => {
     return listCategoriesController.handle(req, res);    
+})
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+    return importCategoryController.handler(req, res);
 })
 
 export{ categoriesRoutes }
