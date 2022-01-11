0780b57216ca2b61c793cb3fea06efa20dfdc110
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-01-11 10:35:10 -0300

    Iniciada a configuração do swagger

--- src/modules/cars/models/useCases/importCategories/ImportCategoryUseCase.ts
+++ src/modules/cars/models/useCases/importCategories/ImportCategoryUseCase.ts
@@ -1,4 +1,4 @@
-import csvParse from "csv-parse";
+import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 import { ICategoriesRepository } from "../../../repositories/ICategoriesRepository";
 
@@ -10,26 +10,45 @@ interface IImportCategory{
 class ImportCategoryUseCase{
     constructor(private categoriesRepository: ICategoriesRepository){}
 
-    loadCategories(file: Express.Multer.File){
-        const stream = fs.createReadStream(file.path);
-        const categories: IImportCategory[] = [];
-        
-        const parseFile = csvParse();
-        
-        stream.pipe(parseFile)
-
-        parseFile.on("data", async (line) => {
-            const [name, description] = line;
-
-            categories.push({
-                name,
-                description,
-            });
-        });
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            
+            const parseFile = csvParse();
+            
+            stream.pipe(parseFile)
+    
+            parseFile.on("data", async (line) => {
+                const [name, description] = line;
+    
+                categories.push({
+                    name,
+                    description,
+                });
+            }).on("end", () => {
+                resolve(categories);
+            }).on("error", (err: any) => {
+                reject(err)
+            })
+        })
     }
 
-    execute(file: Express.Multer.File): void {
-        const categories = this.loadCategories(file);
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        
+        categories.map(async (category) => {
+            const {name, description} = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if(!existCategory){
+                this.categoriesRepository.create({
+                    name, 
+                    description
+                });
+            }
+        });
     }
 }
 
