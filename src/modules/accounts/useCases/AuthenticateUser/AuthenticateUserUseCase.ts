b197d7c7d253ed0f0d9ee807bd55cf85cfa45392
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-01-31 11:29:45 -0300

    Iniciada a implementação do JWT

---
+++ src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,54 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+
+interface IRequest{
+    email: string,
+    password: string
+}
+
+interface IResponse{
+    user: {
+        name: string,
+        email: string
+    };
+    token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+
+    async execute({email, password}: IRequest): Promise<IResponse>{
+        // Verifica se usuário existe
+        const user = await this.usersRepository.findByEmail(email)
+
+        if(!user){
+            throw new Error("Email ou senha incorretos")
+        }
+
+        // Verifica se a senha está correta
+        const passwordMatch = await compare(password, user.password);
+        if(!passwordMatch){
+            throw new Error("Email ou senha incorretos")
+        }
+
+        // Gera o Jsonwebtoken
+        const token = sign({}, "a2932b34ec541c8b99f682f9b813f3b7", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return {
+            user, 
+            token
+        }
+    }
+}
+
+export {AuthenticateUserUseCase}
--- src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt";
+
 
 interface IRequest{
     name: string,
@@ -18,9 +20,20 @@ class CreateUserUseCase{
     ){}
 
     async execute({name, password, email, driver_license}:ICreateUserDTO): Promise<void>{
+        
+        //validando se o email cadastrado já existe
+        const userAlreadyExist = await this.usersRepository.findByEmail(email);
+
+        if(userAlreadyExist){
+            throw new Error("O email já existe em nossos cadastros. Utilize outro email ou faça login")
+        }
+
+        //Criptografando senha no banco
+        const passwordHash = await hash(password, 8)
+        
         await this.usersRepository.create({
             name, 
-            password, 
+            password: passwordHash, 
             email, 
             driver_license
         })
