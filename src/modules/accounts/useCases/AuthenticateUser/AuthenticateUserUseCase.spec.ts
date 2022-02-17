7c518f9f8f6ae7fc46b7a65189575be2fdad5615
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-02-17 20:04:04 -0300

    update testes com jest

--- src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,44 +1,34 @@
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory"
+import { CreateUserUseCase } from "../CreateUser/CreateUserUseCase";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase
 
 describe("Authenticate User", () => {
 
     beforeEach(() => {
-        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
-        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+        usersRepositoryInMemory = new UsersRepositoryInMemory()
+        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
     })
 
-    it("should be able to create a new category", async () => {
-        const category = {
-            name: "Category Test",
-            description: "Category description"
-        }
-
-        await createCategoryUseCase.execute({
-            name: "Category Test",
-            description: "Category description"
+    it("should be able to authenticate an user", async () => {
+        const user: ICreateUserDTO ={
+            driver_license: "01234",
+            email: "teste@teste.com",
+            password: "1234",
+            name: "User Teste"
+        };
+        
+        await createUserUseCase.execute(user);
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password
         })
 
-        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
-
-        expect(categoryCreated).toHaveProperty("id")
-    })
-
-    it("should not be able to create a new category with same name", async () => {
-        expect(async () => {
-            const category = {
-                name: "Category Test",
-                description: "Category description"
-            }
-
-            await createCategoryUseCase.execute({
-                name: "Category Test",
-                description: "Category description"
-            })
-
-            await createCategoryUseCase.execute({
-                name: "Category Test",
-                description: "Category description"
-            })
-        }).rejects.toBeInstanceOf(AppErrors);
+        expect(result).toHaveProperty("token")
     })
 })
