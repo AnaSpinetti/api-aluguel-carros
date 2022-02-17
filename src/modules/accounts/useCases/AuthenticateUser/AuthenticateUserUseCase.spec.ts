284d810c7dd19ce60e5f9db904bf7f2bb150f5bc
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-02-17 12:14:13 -0300

    criação de testes com jest

---
+++ src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,44 @@
+
+
+describe("Authenticate User", () => {
+
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+    })
+
+    it("should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Category description"
+        }
+
+        await createCategoryUseCase.execute({
+            name: "Category Test",
+            description: "Category description"
+        })
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
+
+        expect(categoryCreated).toHaveProperty("id")
+    })
+
+    it("should not be able to create a new category with same name", async () => {
+        expect(async () => {
+            const category = {
+                name: "Category Test",
+                description: "Category description"
+            }
+
+            await createCategoryUseCase.execute({
+                name: "Category Test",
+                description: "Category description"
+            })
+
+            await createCategoryUseCase.execute({
+                name: "Category Test",
+                description: "Category description"
+            })
+        }).rejects.toBeInstanceOf(AppErrors);
+    })
+})
